9c662222996fddf357612b7d9076ed9087fdf0b0
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-05-15 11:57:52 -0700

    css almost fully done

--- src/Adoption.js
+++ src/Adoption.js
@@ -1,34 +1,44 @@
 import React, { Component } from 'react'
+import './Adoption.css'
 
 class Adoption extends Component {
   render() {
   return <div>
-    <h1>Adoption</h1>
-
-    <h3>PET ON STAGE</h3>
-    {/* <img>Good Pet Picture</img> */}
-    <ul>
-      <li>PET NAME: </li>
-      <li>PET GENDER: </li>
-      <li>PET BREED: </li>
-      <li>Physical Description: </li>
-      <li>Story of the Journey to the Shelter: </li>
-    </ul>
-
-    <h3>PEOPLE IN QUEUE</h3>
-    <ul>
-      <li>PERSON 1: </li>
-      <li>PERSON 2: </li>
-      <li>PERSON 3: </li>
-      <li>PERSON 4: </li>
-      <li>PERSON 5: </li>
-    </ul>
-    <form name="addNameToList" id="addNameToList">
-      <label htmlFor="addName"></label>
-      <input type="text" name="addName" id="addName"></input>
-      <button type="submit">ADD ME TO THE QUEUE</button>
-      <button type="submit">ADOPT THAT PET!</button>
-    </form>
+    <header>
+      <h1>Adoption</h1>
+    </header>
+    
+    <main>
+      <section>
+      <h3>PET ON STAGE</h3>
+      <img src="https://images.pexels.com/photos/374906/pexels-photo-374906.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="pug doin work"></img>
+      <ul>
+        <li>PET NAME: </li>
+        <li>PET GENDER: </li>
+        <li>PET BREED: </li>
+        <li>Physical Description: </li>
+        <li>Story of the Journey to the Shelter: </li>
+      </ul>
+      </section>
+      <section>
+      <h3>PEOPLE IN QUEUE</h3>
+      <ul>
+        <li>PERSON 1: </li>
+        <li>PERSON 2: </li>
+        <li>PERSON 3: </li>
+        <li>PERSON 4: </li>
+        <li>PERSON 5: </li>
+      </ul>
+      </section>
+      <section>
+      <form name="addNameToList" id="addNameToList">
+        <label htmlFor="addName"></label>
+        <input type="text" name="addName" id="addName"></input>
+        <button type="submit">ADD ME TO THE QUEUE</button>
+        <button type="submit">ADOPT THAT PET!</button>
+      </form>
+      </section>
+    </main>
   </div>
   }
 }
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './Home.css'
 import { Link } from 'react-router-dom'
 
 class Home extends Component {
@@ -6,18 +7,24 @@ class Home extends Component {
 
   return (
     <div>
-      <h1>Petful</h1>
-      <p>This is an animal shelter for adoption of cats and dogs. The adoption process here works strictly on a "First-In, First-Out" basis. You can adopt a cat, or a dog, or both, but you may only adopt the animal that came to the shelter first. If you want to adopt a new pet, add your name to the list. When it is your turn, you will be able to adopt the currently available pet.</p>
-      <div className="home-dog">
-        <img src="https://images.pexels.com/photos/1254140/pexels-photo-1254140.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="a smiling akita dog"></img>
-        <p>Name: Fran</p>
-        <p>Gender: Female</p>
-        <p>Age: 3</p>
-        <p>Breed: Akita</p>
-        <p>Description: A great hunting dog</p>
-        <p>Story: Found Downtown</p>
-      </div>
-      <div className="home-cat">
+      <header>
+          <h1>PETFUL</h1>
+      </header>
+      
+      <section className="description">
+        This is an animal shelter for adoption of cats and dogs. The adoption process here works strictly on a "First-In, First-Out" basis. You can adopt a cat, or a dog, or both, but you may only adopt the animal that came to the shelter first. If you want to adopt a new pet, add your name to the list. When it is your turn, you will be able to adopt the currently available pet.
+      </section>
+      <main className="container">
+      <section className="home-dog">
+          <img src="https://images.pexels.com/photos/1254140/pexels-photo-1254140.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="a smiling akita dog"></img>
+          <p>Name: Fran</p>
+          <p>Gender: Female</p>
+          <p>Age: 3</p>
+          <p>Breed: Akita</p>
+          <p>Description: A great hunting dog</p>
+          <p>Story: Found Downtown</p>
+      </section>
+      <section className="home-cat">
         <img src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="orange bengal cat laying on concrete"></img>
           <p>Name: Bogo</p>
           <p>Gender: Female</p>
@@ -25,10 +32,13 @@ class Home extends Component {
           <p>Breed: Bengal</p>
           <p>Description: Basically a wild animal</p>
           <p>Story: Found roaming the streets</p>
-      </div>
-      <Link to="/Adoption">
-        <button type='button'>Adopt a pet</button>
-      </Link>
+      </section>
+      </main>
+      <section className="button">
+        <Link to="/Adoption">
+          <button type='button'>ADOPT A PET!</button>
+        </Link>
+      </section>
     </div>
   )
   }
