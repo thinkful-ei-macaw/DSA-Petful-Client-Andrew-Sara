9767d9d13232626123918c3fe35db88de187f68e
Sara R Mills <SaraRMills@gmail.com>
2020-05-15 12:27:07 -0400

    wireframes done

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-//import Adoption from './Adoption'
 
 class Home extends Component {
   render() {
@@ -8,7 +7,7 @@ class Home extends Component {
   return (
     <div>
       <h1>Petful</h1>
-      <p>site description</p>
+      <p>This is an animal shelter for adoption of cats and dogs. The adoption process here works strictly on a "First-In, First-Out" basis. You can adopt a cat, or a dog, or both, but you may only adopt the animal that came to the shelter first. If you want to adopt a new pet, add your name to the list. When it is your turn, you will be able to adopt the currently available pet.</p>
       <div className="home-dog">
         <img src="https://images.pexels.com/photos/1254140/pexels-photo-1254140.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="a smiling akita dog"></img>
         <p>Name: Fran</p>
@@ -20,7 +19,7 @@ class Home extends Component {
       </div>
       <div className="home-cat">
         <img src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="orange bengal cat laying on concrete"></img>
-          <p>Name: Steve French</p>
+          <p>Name: Bogo</p>
           <p>Gender: Female</p>
           <p>Age: 2</p>
           <p>Breed: Bengal</p>
